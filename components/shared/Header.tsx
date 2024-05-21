7a344ac83c8cc00bb5834e7796373c1bf4bf431c
Bonface2 <bonnym2031@gmail.com>
2024-05-21 23:08:12 +0300

    Complete Header, Footer, 1st two pages of homepage

--- components/shared/Header.tsx
+++ components/shared/Header.tsx
@@ -3,6 +3,10 @@ import Image from '@/node_modules/next/image'
 import Link from '@/node_modules/next/link'
 import React from 'react'
 import { Button } from '../ui/button'
+import { SignedIn } from '@clerk/nextjs'
+import { UserButton } from '@clerk/nextjs'
+import NavItems from './NavItems'
+import MobileNav from './MobileNav'
 
 const Header = () => {
   return (
@@ -14,7 +18,16 @@ const Header = () => {
                    alt = "Eventme Logo"
                 /> */}
             </Link> 
+            <SignedIn>
+              <nav className = "md:flex-between hidden w-full max-w-xs" >
+                <NavItems/>
+              </nav>
+            </SignedIn>
             <div className='flex w-32 justify-end gap-3'>
+              <SignedIn>
+                <UserButton afterSignOutUrl = "/"/>
+                <MobileNav/>
+              </SignedIn>
                 <SignedOut>
                     <Button asChild className='rounded-full' size = "lg">
                       <Link href = "/sign-in">
