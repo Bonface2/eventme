4c4aba59dd9b1cfc7f15c9dc1fcb10ed62d2e124
Bonface2 <bonnym2031@gmail.com>
2024-07-17 18:22:36 +0300

    Activate Logo in Header

--- components/shared/Header.tsx
+++ components/shared/Header.tsx
@@ -13,10 +13,10 @@ const Header = () => {
     <header className = 'w-full  border-b'>
         <div className= 'wrapper flex items-center justify-between'>
             <Link href="/" className = "w-36">
-                {/* <Image
-                   src = "assets/images/logo.svg" width = {128} height = {38}
+                <Image
+                   src = "Assets/Images/logo.svg" width = {128} height = {38}
                    alt = "Eventme Logo"
-                /> */}
+                />
             </Link> 
             <SignedIn>
               <nav className = "md:flex-between hidden w-full max-w-xs" >
